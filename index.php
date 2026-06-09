c25b69fa3fac10d4f68f65e3aa11f1a708d048a7
angeloren92 <angeloren92@gmail.com>
2026-06-09 11:31:28 +0200

    add cards

--- index.php
+++ index.php
@@ -48,26 +48,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
 
-    <?php
+    <header>
+<!-- As a link -->
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+            </div>
+        </nav>
+    </header>
 
-    foreach ($hotels as $hotel) {
+    <main class="container py-5">
 
-        echo $hotel["name"];
-        echo "<br/>";
-        echo $hotel["description"];
-        echo "<br/>";
-        echo $hotel["parking"];
-        echo "<br/>";
-        echo $hotel["vote"];
-        echo "<br/>";
-        echo $hotel["distance_to_center"];
+    <section class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
+        <?php foreach ($hotels as $hotel): ?>
+            <div class="col">
+                <div class="card">
+                    <div class="card-title">
+                        <h2 class="p-3">
+                            <?php echo $hotel["name"]; ?>
+                        </h2>
+                    </div>
+                    <div class="card-body">
+                        <p>
+                            <?php echo $hotel["description"]; ?>
+                        </p>
 
-    }
 
-    ?>
+                        
+                    </div>
+                </div>        
+            </div>
+        <?php endforeach; ?>
+
+    </section>
+
+    </main>
     
+    <footer>
+
+    </footer>
+
 </body>
 </html>
